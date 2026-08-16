61a0fb3b7df561a4ab0df513f76f0f8390272a7e
Ink-Lure <jamesbagray@gmail.com>
2026-08-16 15:46:56 +0100

    Add: string-encode-and-decode - submission-23

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-23.py
@@ -0,0 +1,7 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return ''.join(f"/{len(x)}.{x}" for x in strs)
+
+    def decode(self, s: str) -> List[str]:
+        return [s[i:l+i] for (i,l) in [(i + re.search("^/[0-9]+.", s[i:]).span()[1], int(re.search("^/[0-9]+.", s[i:]).group()[1:-1])) for i, x in enumerate(s) if x == "/" and bool(re.search("^/[0-9]+.", s[i:]))]]
